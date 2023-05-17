ad9632bc6f63305ff101f27441b85caa89b83099
Konrad <konrad22.02@wp.pl>
2023-05-17 09:11:29 +0200

    CORS policy

--- server/index.js
+++ server/index.js
@@ -44,6 +44,8 @@ app.use(function (req, res, next) {
   // Website you wish to allow to connect
   res.setHeader("Access-Control-Allow-Origin", "*");
 
+  res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+
   // Request methods you wish to allow
   res.setHeader(
     "Access-Control-Allow-Methods",
