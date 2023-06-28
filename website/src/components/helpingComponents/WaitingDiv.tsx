b6fc852f2a90d480e923b0e9148e972c15edb00e
szariii <76693916+szariii@users.noreply.github.com>
2023-06-28 22:22:35 +0200

    Update WaitingDiv.tsx

--- website/src/components/helpingComponents/WaitingDiv.tsx
+++ website/src/components/helpingComponents/WaitingDiv.tsx
@@ -15,8 +15,8 @@ const WaitingDiv = () => {
 
 const WaitingDivStyle = styled.div`
   position: fixed;
-  width: 100%;
-  height: 100%;
+  width: 100vw;
+  height: 100vh;
   background-color: rgba(0, 0, 0, 0.4);
   display: flex;
   align-items: center;
